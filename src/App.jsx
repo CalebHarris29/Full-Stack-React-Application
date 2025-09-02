91f139b16efc05d12184887152ed1f59a42b261a
CalebHarris29 <144962655+CalebHarris29@users.noreply.github.com>
2025-09-02 11:39:25 -0400

    Update landing text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-// src/App.jsx
 import './App.css';
 
 export default function App() {
   return (
     <main style={{ padding: 24 }}>
       <h1>NotesApp — deployed on Amplify ✅</h1>
-      <p>If you see this text on your Amplify URL, your CI/CD pipeline works.</p>
+      <p>If you see this on your Amplify URL, CI/CD is working.</p>
     </main>
   );
 }
-
